58fcb471595499c22f36c485255221e221092a59
exact-coder <hasanakash799@gmail.com>
2025-05-03 02:41:35 +0600

    chore: added custom button and swiper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,8 +1,10 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from 'expo-router';
 import Swiper from 'react-native-swiper';
+import { onboarding } from '@/constants';
+import CustomButton from '@/components/CustomButton';
 
 const Onboarding = () => {
     const swiperRef = useRef<Swiper>(null);
@@ -13,17 +15,44 @@ const Onboarding = () => {
         <Text className='text-black text-md font-JakartaBold'>Skip</Text>
       </TouchableOpacity>
 
-      {/* <Swiper 
+      <Swiper
         ref={swiperRef}
         loop={false}
-        dot={<View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full' />}
-        activeDot={<View className='w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full' />}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
         onIndexChanged={(index) => setActiveIndex(index)}
       >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
 
-      </Swiper> */}
+      <CustomButton 
+        title={"Next"}
+        className={"w-11/12 mt-10"}
+      />
     </SafeAreaView>
   )
 }
 
 export default Onboarding;
+
+// Your doing lengeth-1.09
